c84723d106bfce6b6b67ba02027edc0629f9ec74
agorbacheva-y <agorbacheva.y@gmail.com>
2023-08-18 15:07:40 +0200

    Chat component

--- src/components/Chat.js
+++ src/components/Chat.js
@@ -1,10 +1,77 @@
-import React from 'react';
+import { useEffect, useState } from "react";
 import "./Chat.css";
+import { Avatar, IconButton } from '@mui/material';
+import SearchOutlined from '@mui/icons-material/SearchOffOutlined';
+import AttachFile from "@mui/icons-material/AttachFile";
+import MoreVert from '@mui/icons-material/MoreVert';
+import InsertEmoticon from "@mui/icons-material/InsertEmoticon";
+import MicIcon from "@mui/icons-material/Mic";
 
 function Chat() {
+  const [ seed, setSeed ] = useState("");
+  const [ input, setInput ] = useState("");
+
+  // generate random avator for each chat room
+  useEffect(() => {
+    setSeed(Math.floor(Math.random() *5000));
+  }, []);
+
+  const sendMessage = (e) => {
+    e.preventDefault();
+    console.log("You typed >>> ", input);
+  };
+
   return (
     <div className='chat'>
-      
+      <div className='chat__header'>
+        <Avatar src={`https://avatars.dicebear.com/api/human/${seed}.svg`}/>
+
+        <div className="chat__headerInfo">
+          <h3>Room name</h3>
+          <p>Last seen at...</p>
+        </div>
+
+        <div className="chat__headerRight">
+          <IconButton> 
+            <SearchOutlined />
+          </IconButton>
+
+          <IconButton> 
+            <AttachFile />
+          </IconButton>
+          
+          <IconButton>
+            <MoreVert />
+          </IconButton>
+        </div>
+      </div>
+
+      <div className='chat__body'>
+        <p className={`chat__message ${true && "chat__receiver"}`}>
+          {/* user who logs in will evaluate to true */}
+          <span className="chat__name">name</span>
+          Hej
+          <span className="chat__timestamp">time stamp</span>
+        </p>
+        
+      </div>
+
+      <div className='chat__footer'>
+        <InsertEmoticon />
+        <form>
+          <input  type="text" 
+                  placeholder="Type message" 
+                  value={input}
+                  onChange={(e) => setInput(e.target.value)}
+            />
+          <button type="submimt"
+                  onClick={sendMessage}
+            >
+              Send message
+          </button>
+        </form>
+        <MicIcon />
+      </div>
     </div>
   );
 };
